b143d2f35b0f32cabe39f4a5fb47dd83b6efda90
unknown <sarath.pondreti93@gmail.com>
2026-07-01 23:33:49 +0530

    routerconfig changed

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <BrowserRouter basename="/tanishqgreen">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/our-pillars" element={<OurPillars />} />
