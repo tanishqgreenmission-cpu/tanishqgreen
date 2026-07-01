4c81b9819a6f2deda2188cce3a5f268930e605c8
unknown <sarath.pondreti93@gmail.com>
2026-07-01 20:15:11 +0530

    app.jsx changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,14 @@ import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/tanishqgreen">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/our-pillars" element={<OurPillars />} />
         <Route path="/action-hub" element={<ActionHub />} />
         <Route path="/impact" element={<Impact />} />
         <Route path="/contact" element={<Contact />} />
-        {/* Legacy redirects */}
+
         <Route path="/about" element={<OurPillars />} />
         <Route path="/our-work" element={<ActionHub />} />
         <Route path="/champions" element={<Impact />} />
