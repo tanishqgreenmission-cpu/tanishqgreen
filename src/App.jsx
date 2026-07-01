bdea3fcdb61e4beae7ecc684aeef2fb26e635a9b
unknown <sarath.pondreti93@gmail.com>
2026-07-01 23:42:18 +0530

    router issue fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/our-pillars" element={<OurPillars />} />
